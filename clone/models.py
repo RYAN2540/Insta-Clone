cf36d0639225261b2a2a1b13ad7acacd0d02acf4
Your Name <you@example.com>
2021-03-26 15:28:51 +0300

    create image model

--- clone/models.py
+++ clone/models.py
@@ -2,3 +2,12 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Image(models.Model):
+    image = CloudinaryField('image')
+    name = models.CharField(max_length = 30)
+    caption = models.TextField(blank= True)
+    post_date = models.DateTimeField(auto_now_add=True)
+    profile = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name="posts")
+
+
