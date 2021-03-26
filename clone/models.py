e0dcb618ebd54802aa04fcf98bb3bb8b27e10c5e
Your Name <you@example.com>
2021-03-26 15:44:03 +0300

    add comment model

--- clone/models.py
+++ clone/models.py
@@ -44,3 +44,22 @@ class Profile(models.Model):
     @classmethod
     def search_user(cls,username): 
         return User.objects.filter(username__icontains = username)
+
+class Comment(models.Model):
+    content = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, on_delete= models.CASCADE, related_name = "comments")
+
+    def __str__(self):
+        return self.content
+
+    def save_comment(self):
+        self.save()
+
+    def delete_comment(self):
+        self.delete()
+
+    @classmethod
+    def get_image_comments(cls,image):
+        return cls.objects.filter(image =image)
