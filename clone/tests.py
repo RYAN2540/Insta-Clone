f75a6545a37c972f5f5b189913e147b845656481
Your Name <you@example.com>
2021-03-26 15:55:29 +0300

    add tests for image model

--- clone/tests.py
+++ clone/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
+from .models import Image, Comment, Profile
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class ImageTestClas(TestCase):
+    def setUp(self):
+        self.lorna = User(username = "ryan", email = "austinbrian005@gmail.com",password = "1234")
+        self.food = Image(image = 'imageurl', name ='food', caption = 'Chicken Taco', profile = self.lorna)
+        self.maua = Image(image = 'imageurl', name ='maua', caption = 'Lillies', profile = self.lorna)
+
+        self.lorna.save()
+        self.food.save_image()
+
+    def tearDown(self):
+        Image.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.food, Image))
+
+    def test_save_image_method(self):
+        images = Image.objects.all()
+        self.assertTrue(len(images)> 0)
+
+    def test_delete_image(self):
+        images1 = Image.objects.all()
+        self.assertEqual(len(images1),1)
+        self.food.delete_image()
+        images2 = Image.objects.all()
+        self.assertEqual(len(images2),0)
+
+    def test_update_caption(self):
+        self.food.update_caption('Bolognese')
+        self.assertEqual(self.food.caption, 'Bolognese')
+
+    def test_get_profile_images(self):
+        self.maua.save_image()
+        images = Image.get_profile_images(self.lorna)
+        self.assertEqual(len(images),2)
+
+
