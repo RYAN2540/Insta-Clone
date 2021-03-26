18a1fbb6400e52d7aa1e1d100456e2f444582051
Your Name <you@example.com>
2021-03-26 15:33:35 +0300

    add image model methods

--- clone/models.py
+++ clone/models.py
@@ -10,4 +10,25 @@ class Image(models.Model):
     profile = models.ForeignKey(User, on_delete=models.CASCADE)
     likes = models.ManyToManyField(User, related_name="posts")
 
+    def __str__(self):
+        return self.name
+    
+    def save_image(self):
+        self.save()
 
+    def delete_image(self):
+        self.delete()
+
+    def update_caption(self, new_caption):
+        self.caption = new_caption
+        self.save()
+
+    def like_count(self):
+        return self.likes.count()
+
+    @classmethod
+    def get_profile_images(cls,profile):
+        return cls.objects.filter(profile = profile)
+
+    class Meta:
+        ordering = ['-post_date']
