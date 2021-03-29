9ce7a24d94a0c9a427e0ca517ff34485f841f35c
Your Name <you@example.com>
2021-03-29 12:26:53 +0300

    add edit profile url

--- clone/urls.py
+++ clone/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns=[
     url('^$',views.home,name = 'home'),
     url(r'^profile/$',views.profile,name = 'profile'),
-    url(r'^upload/image/$', views.upload_image, name = "upload_image"),
+    url(r'^upload/image$', views.upload_image, name = "upload_image"),
     url(r'^search/',views.search,name ='search'),
     url(r'^comment/(?P<image_id>\d+)', views.comment,name = "comment"),
+    url(r'^profile/edit$', views.edit_profile,name = 'edit_profile'),
 ]
