774af31e8b4b3718876d94f66335ddad44e8d0f2
Your Name <you@example.com>
2021-03-29 14:57:12 +0300

    add url for like image

--- clone/urls.py
+++ clone/urls.py
@@ -6,6 +6,7 @@ urlpatterns=[
     url(r'^profile/$',views.profile,name = 'profile'),
     url(r'^upload/image$', views.upload_image, name = "upload_image"),
     url(r'^search/',views.search,name ='search'),
+    url(r'^like/<int:id>$', views.like_image, name = 'like_image'),
     url(r'^comment/(?P<image_id>\d+)', views.comment,name = "comment"),
     url(r'^profile/edit$', views.profile_edit,name = 'profile_edit'),
 ]
